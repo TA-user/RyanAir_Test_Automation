ff794d76a52985c9753811fecde0989d7b5a4e5b
yefim-mironyuk <efimmironyk@gmail.com>
2022-01-12 17:22:28 +0300

    Small changes

--- utils/logging.py
+++ utils/logging.py
@@ -14,7 +14,6 @@ class Logger:
         logger.remove()
         return logger.add("logs/debug.log",
                           format="{time:DD:MM:YYYY:h:m:s} {level} {message}",
-                          level="DEBUG",
                           rotation="10 KB",
                           retention="2 days",
                           filter=self.debug_only), \
