b3f0cd99ac12df71704860917edadb94d5794ac9
solenok.egor <solenok.egor@gmail.com>
2022-01-12 16:43:04 +0300

    restore to selenium locators

--- tests/pages/main_page_locators.py
+++ tests/pages/main_page_locators.py
@@ -1,48 +1,49 @@
-from selene import by
+from selenium.webdriver.common.by import By
 
 
 class LogInPopupLocators:
-    CONFIRMATION_LOG_IN_BUTTON = (by.xpath("//form[@data-ref='login_modal']//button[@type='submit']"))
-    EMAIL_FORM = (by.xpath("//input[@placeholder='email@email.com']"))
-    PASSWORD_FORM = (by.xpath("//input[@placeholder='Password']"))
+    CONFIRMATION_LOG_IN_BUTTON = (By.XPATH, "//form[@data-ref='login_modal']//button[@type='submit']")
+    EMAIL_FORM = (By.XPATH, "//input[@placeholder='email@email.com']")
+    PASSWORD_FORM = (By.XPATH, "//input[@placeholder='Password']")
 
 
 class MainPageLocators:
-    ACCEPT_COOKIES_BUTTON = (by.xpath("""//div[@id='cookie-popup-with-overlay']//button
-        [@class='cookie-popup-with-overlay__button']"""))
-    FRAME_DATEPICKER = (by.xpath("//iframe[@id='_hjRemoteVarsFrame']"))
-    SEARCH_CARS_TAB = (by.xpath("//hp-search-widget-tab[@iconid='glyphs/cars']"))
-    SEARCH_FLIGHTS_TAB = (by.xpath("//hp-search-widget-tab[@iconid='glyphs/plane']"))
-    SEARCH_HOTELS_TAB = (by.xpath("//hp-search-widget-tab[@iconid='glyphs/hotels']"))
+
+    ACCEPT_COOKIES_BUTTON = (By.XPATH, """//div[@id='cookie-popup-with-overlay']//button
+        [@class='cookie-popup-with-overlay__button']""")
+    FRAME_DATEPICKER = (By.XPATH, "//iframe[@id='_hjRemoteVarsFrame']")
+    SEARCH_CARS_TAB = (By.XPATH, "//hp-search-widget-tab[@iconid='glyphs/cars']")
+    SEARCH_FLIGHTS_TAB = (By.XPATH, "//hp-search-widget-tab[@iconid='glyphs/plane']")
+    SEARCH_HOTELS_TAB = (By.XPATH, "//hp-search-widget-tab[@iconid='glyphs/hotels']")
 
 
 class MainPageHeaderLocators:
-    LOG_IN_BUTTON = (by.xpath("//button[@aria-label='Log in']"))
-    LOG_OUT_BUTTON = (by.xpath("//a[@data-ref='header-dropdown-user__logout']"))
-    USER_MENU = (by.xpath("//button[@data-ref='header-menu-item__toggle-button']/hp-header-menu-user"))
+    LOG_IN_BUTTON = (By.XPATH, "//button[@aria-label='Log in']")
+    LOG_OUT_BUTTON = (By.XPATH, "//a[@data-ref='header-dropdown-user__logout']")
+    USER_MENU = (By.XPATH, "//button[@data-ref='header-menu-item__toggle-button']/hp-header-menu-user")
 
 
 class SearchCarsTabLocators:
-    CAR_LOCATION = (by.xpath("//*[@id='input-button__pick-up-location']"))
-    DROP_OFF_FORM = (by.xpath("//hp-input-button[@uniqueid='drop-off-date']"))
-    DROP_OFF_TIME_FORM = (by.xpath("//hp-input-button[@uniqueid='drop-off-time']"))
-    FIRST_ITEM_LOCATION_DROPDOWN = (by.xpath("//hp-car-hire-location/div[1]"))
-    PICK_UP_FORM = (by.xpath("//hp-input-button[@uniqueid='pick-up-date']"))
-    PICK_UP_TIME_FORM = (by.xpath("//hp-input-button[@uniqueid='pick-up-time']"))
-    SEARCH_CARS_BUTTON = (by.xpath("//button[@data-ref='car-hire-widget__cta']"))
+    CAR_LOCATION = (By.XPATH, "//*[@id='input-button__pick-up-location']")
+    DROP_OFF_FORM = (By.XPATH, "//hp-input-button[@uniqueid='drop-off-date']")
+    DROP_OFF_TIME_FORM = (By.XPATH, "//hp-input-button[@uniqueid='drop-off-time']")
+    FIRST_ITEM_LOCATION_DROPDOWN = (By.XPATH, "//hp-car-hire-location/div[1]")
+    PICK_UP_FORM = (By.XPATH, "//hp-input-button[@uniqueid='pick-up-date']")
+    PICK_UP_TIME_FORM = (By.XPATH, "//hp-input-button[@uniqueid='pick-up-time']")
+    SEARCH_CARS_BUTTON = (By.XPATH, "//button[@data-ref='car-hire-widget__cta']")
 
 
 class SearchFlightsTabLocators:
-    INPUT_DEPARTURE_FORM = (by.xpath("//*[@id='input-button__departure']"))
-    INPUT_DESTINATION_FORM = (by.xpath("//input[@id='input-button__destination']"))
-    LAST_ITEM_DROPDOWN_AIRPORT = (by.xpath("(//span[@data-ref='airport-item__name'])[last()]"))
-    RETURN_FORM = (by.xpath("//fsw-input-button[@uniqueid='dates-to']"))
-    SEARCH_FLIGHT_BUTTON = (by.xpath("//button[@data-ref='flight-search-widget__cta']"))
+    INPUT_DEPARTURE_FORM = (By.XPATH, "//*[@id='input-button__departure']")
+    INPUT_DESTINATION_FORM = (By.XPATH, "//input[@id='input-button__destination']")
+    LAST_ITEM_DROPDOWN_AIRPORT = (By.XPATH, "(//span[@data-ref='airport-item__name'])[last()]")
+    RETURN_FORM = (By.XPATH, "//fsw-input-button[@uniqueid='dates-to']")
+    SEARCH_FLIGHT_BUTTON = (By.XPATH, "//button[@data-ref='flight-search-widget__cta']")
 
 
 class SearchHotelsTabLocators:
-    CHECK_IN_FORM = (by.xpath("//hp-input-button[@uniqueid='check-in']"))
-    CHECK_OUT_FORM = (by.xpath("//hp-input-button[@uniqueid='check-out']"))
-    FIRST_ITEM_HOTEL_DROPDOWN = (by.xpath("//icon[@iconid='glyphs/destination-pin']"))
-    DESTINATION_HOTEL_FORM = (by.xpath("//*[@id='input-button__locations-or-properties']"))
-    SEARCH_HOTEL_BUTTON = (by.xpath("//button[@data-ref='rooms-search-widget__cta']"))
+    CHECK_IN_FORM = (By.XPATH, "//hp-input-button[@uniqueid='check-in']")
+    CHECK_OUT_FORM = (By.XPATH, "//hp-input-button[@uniqueid='check-out']")
+    FIRST_ITEM_HOTEL_DROPDOWN = (By.XPATH, "//icon[@iconid='glyphs/destination-pin']")
+    DESTINATION_HOTEL_FORM = (By.XPATH, "//*[@id='input-button__locations-or-properties']")
+    SEARCH_HOTEL_BUTTON = (By.XPATH, "//button[@data-ref='rooms-search-widget__cta']")
