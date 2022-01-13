1a8be3d4b551c00848dd8c469ffc96afcd89caf2
Daniil <kit-91-daniil@mail.ru>
2022-01-13 17:39:29 +0300

    fix BasePage constructor(timeout)

--- tests/pages/base_page.py
+++ tests/pages/base_page.py
@@ -2,10 +2,9 @@ from utils.browser_helper import ElementInteractions
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=7):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.timeout = timeout
         self.element_interactions = ElementInteractions(self.browser)
 
     def open(self):
