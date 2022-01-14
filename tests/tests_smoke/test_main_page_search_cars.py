4958fce8899ed5f567e4674f59b76621148123c3
yefim-mironyuk <efimmironyk@gmail.com>
2022-01-14 17:54:27 +0300

    we decided to make smoke tests in one file smoke_test

--- tests/tests_smoke/test_main_page_search_cars.py
+++
@@ -1,5 +0,0 @@
-def test_user_can_hire_car(browser, main_page, main_page_car_hire_tab):
-    main_page.open()
-    main_page.click_accept_cookie_popup_button()
-    main_page.open_search_cars_tab()
-    main_page_car_hire_tab.perform_car_sharing("Berlin", "2022-04-10", "10:00", "2022-04-11", "10:00")
